b9cf9920dd22b0ce5a8afed85b548220880946b9
Abominate34 <92999356+Abominate34@users.noreply.github.com>
2022-03-05 19:55:24 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-input('test version1>> ')
+import requests 
+import os
+
+version = open('version').read()
+newest = requests.get('https://raw.githubusercontent.com/Abominate34/Test_repo/main/version.txt').text.split('\n')[0]
+
+
+if version == newest:
+	print("Current version is newest")
+
+else:
+	print('old_version')
+	f=open(r'main.py',"wb") 
+	ufr = requests.get("https://raw.githubusercontent.com/Abominate34/Test_repo/main/test.py") 
+	f.write(ufr.content)
+	f.close()
+	f=open(r'version',"wb").write(newest).close()
