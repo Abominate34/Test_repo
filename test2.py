6fba2535d9686f36a05039ae37c0db674c2cb765
Abominate34 <92999356+Abominate34@users.noreply.github.com>
2022-03-10 23:26:40 +0300

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,32 @@
+import requests 
+import os
+import sys
+import subprocess
+#ver1
+
+
+
+def checkupdates():
+	print('Проверка обновлений...')
+
+	version_link = 'https://raw.githubusercontent.com/Abominate34/Test_repo/main/version'
+	code_link = "https://raw.githubusercontent.com/Abominate34/Test_repo/main/test.py"
+	file_to_restart = "test.py"
+
+	version = open('version').read().split('\n')[0]
+	newest = requests.get(version_link)
+
+
+	if version == newest.text.split('\n')[0]:
+		print("[+] У вас самая новая версия!")
+
+	else:
+		print('[-] Версия устарела! Обновление...')
+		f=open(file_to_restart,"wb") 
+		ufr = requests.get(code_link) 
+		f.write(ufr.content)
+		f.close()
+		f=open(r'version',"wb").write(newest.content)
+		os.execvp(sys.executable,[sys.executable,file_to_restart,],)
+
+checkupdates()
